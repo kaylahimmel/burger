f8644170c9b5bc96803d7aa1089ab8cdc275179d
Kayla Himmelberger <kaylahimmelberger@gmail.com>
2018-11-02 10:52:46 -0400

    added server.js, schema.sql, and seeds.sql files

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+SELECT * FROM burgers;
+
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(40) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Royal Red Robin Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Black & Bleu Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Smoke & Pepper Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Sauteed 'Shroom Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Banzai Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Guacamole & Bacon Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Wedgie Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Citrus Harissa Salmon Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Crispy Artic Cod Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Grilled Turkey Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Marco Pollo Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bruschetta Chicken Burger", false);
