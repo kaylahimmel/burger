5a1df66ab9fb02f06f6945294970e1f42849f25d
Kayla Himmelberger <kaylahimmelberger@gmail.com>
2018-11-06 21:48:04 -0500

    updated connections.js page

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,19 @@
 // requirements/dependencies
 var mysql = require("mysql");
-
+var connection;
 
 // setup the code to connect Node to MySQL
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 8889,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+};
 
 // make the connection
 connection.connect(function(err) {
